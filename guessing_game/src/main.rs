5002dd9ff88ac62333624c530589b8880e0af77d
ClassErased <aaron@aacyber.uk>
2022-06-05 19:50:54 +0100

    check users input vs generated number

--- guessing_game/src/main.rs
+++ guessing_game/src/main.rs
@@ -1,10 +1,9 @@
 use std::io;
 use rand::Rng;
+use std::cmp::Ordering;
 
 fn main() {
-
-    let secret_number = rand::thread_rng().gen_range(1..101);
-
+    
     println!("Guess the number!");
     println!("Please input your guess.");
 
@@ -14,6 +13,16 @@ fn main() {
         .read_line(&mut guess)
         .expect("Failed to read line");
 
+    let guess: u32 = guess.trim().parse().expect("Please type a number!");
+    
+    let secret_number = rand::thread_rng().gen_range(1..101);
+
     println!("You guessed: {}", guess);
-    println!("The number was {}", secret_number);
+    // println!("The number was {}", secret_number);
+
+    match guess.cmp(&secret_number) {
+        Ordering::Less => println!("Too small!"),
+        Ordering::Greater => println!("Too big!"),
+        Ordering::Equal => println!("You win!"),
+    }
 }
