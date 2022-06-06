04a17e77326f0b68cd51e000a55aac68e067bb24
ClassErased <aaron@aacyber.uk>
2022-06-06 22:50:38 +0100

    messing around

--- functions/src/main.rs
+++ functions/src/main.rs
@@ -2,11 +2,19 @@ use std::io;
 
 //just playing around, worked out why it works, trying to test input
 fn main() {
-    let mut print_labeled_measurement = String::new();
-    io::stdin().read_line(&mut print_labeled_measurement).expect("Failed to read line");
+    let mut labeled_measurement = String::new();
+    io::stdin().read_line(&mut labeled_measurement).expect("Failed to read line");
+    
 }
 
-fn print_labeled_measurement(value: i32, value2: i32) {
-    println!("The measurement is: {}", value * value2);
+fn statics(){
+    let value2 = 1.3;
+    
+
+
+}
+
+fn print_labeled_measurement(labeled_measurement: f32, value2: f32) {
+    println!("The measurement is: {}", labeled_measurement * value2);
 }
 
