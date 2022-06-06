8fb9b6381d1850a7b42c6cfeb76c68e0c2f59b07
ClassErased <aaron@aacyber.uk>
2022-06-06 18:07:36 +0100

    wtf

---
+++ functions/src/main.rs
@@ -0,0 +1,8 @@
+fn main() {
+    print_labeled_measurement(5, 1);
+}
+//i dont have a clue why this works
+fn print_labeled_measurement(value: i32, vlue: i32) {
+    println!("The measurement is: {}", value * vlue);
+}
+
