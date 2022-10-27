fc5958bf37b8dc90e50eee1287266612731be3c7
ClassErased <aaron@aacyber.uk>
2022-10-27 15:57:19 +0100

    what a mess

--- vectors/src/main.rs
+++ vectors/src/main.rs
@@ -1,11 +1,14 @@
-//A Vector is basically a dynamically sized array, values can be pushed and changed.
-//Unlike arrays where they only be replaced.
+//A Vector is basically a dynamically sized array, new values can be pushed and added.
+//Unlike arrays where they may only be replaced.
 
 pub fn run() {
-    let numbers: Vec<i32> = vec![2, 4, 6, 8 ,10];
+    let mut numbers: Vec<i32> = vec![2, 4, 6, 8 ,10];
 
+    // Vec::insert 
     // use debug trait to print vector
     println!("{:?}", numbers);
+
+    //assert_eq!(numbers, [2, 3, 4]);
 }
 
 fn main() {
